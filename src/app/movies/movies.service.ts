8fa0cf162786230a8b00d074369f4892b24920ab
Fabián Gómez Campo <fabian.campo@icloud.com>
2025-11-23 00:25:41 +0100

    feat: movies api call

---
+++ src/app/movies/movies.service.ts
@@ -0,0 +1,18 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Movie } from './interfaces/movie.interface';
+
+const baseUrl = environment.baseUrl;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MoviesService {
+  private http = inject(HttpClient);
+
+  getMovies(): Observable<Movie[]> {
+    return this.http.get<Movie[]>(`${baseUrl}/films`);
+  }
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, inject } from '@angular/core';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+} from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { MoviesService } from '../movies/movies.service';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    ExploreContainerComponent,
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+  ],
 })
 export class Tab1Page {
-  constructor() {}
+  moviesService = inject(MoviesService);
+
+  moviesResource = rxResource({
+    params: () => ({}),
+    stream: () => this.moviesService.getMovies(),
+  });
 }
