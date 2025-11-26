738ab6aec380bc059c654240d5f160dcd1c00f05
Fabián Gómez Campo <fabian.campo@icloud.com>
2025-11-26 11:39:10 +0100

    refactor: tabs layout

--- src/app/layout/tabs/tabs.routes.ts
+++ src/app/layout/tabs/tabs.routes.ts
@@ -9,28 +9,28 @@ export const routes: Routes = [
       {
         path: 'movies',
         loadComponent: () =>
-          import('../features/movies/pages/movies-list/movies-list.page').then(
-            (m) => m.MoviesListPage
-          ),
+          import(
+            '../../features/movies/pages/movies-list/movies-list.page'
+          ).then((m) => m.MoviesListPage),
       },
       {
         path: 'movie/:id',
         loadComponent: () =>
           import(
-            '../features/movies/pages/movie-detail/movie-detail.page'
+            '../../features/movies/pages/movie-detail/movie-detail.page'
           ).then((m) => m.MovieDetailPage),
       },
       {
         path: 'favorites',
         loadComponent: () =>
           import(
-            '../features/favorites/pages/favorites-list/favorites-list.page'
+            '../../features/favorites/pages/favorites-list/favorites-list.page'
           ).then((m) => m.FavoritesListPage),
       },
       {
         path: 'settings',
         loadComponent: () =>
-          import('../features/settings/pages/settings/settings.page').then(
+          import('../../features/settings/pages/settings/settings.page').then(
             (m) => m.SettingsPage
           ),
       },
